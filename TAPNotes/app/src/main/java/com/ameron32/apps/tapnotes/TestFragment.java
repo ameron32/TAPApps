c07a78313d5c0ec887feb1f6a32df086b4d62896
ameron32 <ameron32extras@gmail.com>
2015-02-23 16:39:38 -0600

    add CrossFade-SPL

--- TAPNotes/app/src/main/java/com/ameron32/apps/tapnotes/TestFragment.java
+++ TAPNotes/app/src/main/java/com/ameron32/apps/tapnotes/TestFragment.java
@@ -1,26 +1,24 @@
 package com.ameron32.apps.tapnotes;
 
 
-import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SlidingPaneLayout;
+import android.view.View;
 
-import com.parse.ParseObject;
-
-public class TestFragment extends AbsContentFragment {
+public class TestFragment extends AbsContentFragment
+{
   @Override
   protected int getCustomLayoutResource() {
-    return R.layout.view_spacer;
+//    return R.layout.view_spacer;
+    return R.layout.trial_fragment_test;
   }
 
   @Override
-  public void onAttach(Activity activity) {
-    super.onAttach(activity);
-
-    testParse();
-  }
-
-  private void testParse() {
-    ParseObject testObject = new ParseObject("TestObject");
-    testObject.put("key", "value");
-    testObject.saveInBackground();
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+//    final SlidingPaneLayout layout = (SlidingPaneLayout) view.findViewById(R.id.sliding_pane_layout);
+//    layout.setSliderFadeColor(Color.TRANSPARENT);
   }
 }
