4c10300dca27af366047e0f051af92d6621888e4
ameron32 <ameron32extras@gmail.com>
2015-04-06 17:06:10 -0500

    minor changes

--- TAPNotes/app/src/main/java/com/ameron32/apps/tapnotes/di/controller/ActivitySnackBarController.java
+++ TAPNotes/app/src/main/java/com/ameron32/apps/tapnotes/di/controller/ActivitySnackBarController.java
@@ -1,8 +1,10 @@
 package com.ameron32.apps.tapnotes.di.controller;
 
 import android.app.Activity;
+import android.widget.Toast;
 
-import com.kenny.snackbar.SnackBar;
+import com.ameron32.apps.tapnotes.R;
+import com.gc.materialdesign.widgets.SnackBar;
 
 /**
  * Created by klemeilleur on 3/2/2015.
@@ -14,6 +16,20 @@ public class ActivitySnackBarController extends AbsController {
   }
 
   public void toast(String message) {
-    SnackBar.show(getActivity(), message);
+    // from package com.kenny.snackbar.SnackBar
+//    SnackBar.show(getActivity(), message);
+
+    // from package com.gc.materialdesign.widgets.SnackBar
+    new SnackBar(getActivity(), message).show();
+
+    //
+//    com.rey.material.widget.
+//    SnackBar.make(getActivity())
+//        .applyStyle(R.style.Material_Widget_SnackBar)
+//        .text(message)
+//        .show();
+
+    // fallback
+//    Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
   }
 }
--- TAPNotes/app/src/main/java/com/ameron32/apps/tapnotes/unused/ContentManager.java
+++ TAPNotes/app/src/main/java/com/ameron32/apps/tapnotes/unused/ContentManager.java
@@ -1,4 +1,4 @@
-package com.ameron32.apps.tapnotes;
+package com.ameron32.apps.tapnotes.unused;
 
 /*********************************************************************
  * *******************************************************************
@@ -13,6 +13,8 @@ package com.ameron32.apps.tapnotes;
 
 import android.support.v4.app.Fragment;
 
+import com.ameron32.apps.tapnotes.AbsContentFragment;
+import com.ameron32.apps.tapnotes.R;
 import com.ameron32.apps.tapnotes._trial._demo.FragmentProvider;
 import com.ameron32.apps.tapnotes._trial._demo.fragment.MaterialImageViewTestFragment;
 import com.ameron32.apps.tapnotes._trial._demo.fragment.TestFragment;
